586401fcf2803c3e2b668ea4f6a9d65bd11c0abf
Md. Ebrahim Hossain <ebrahimhossaincse43@gmail.com>
2022-03-18 13:21:09 +0600

    dropdownhandling

--- cypress/integration/Basics/DropDownHandling.js
+++ cypress/integration/Basics/DropDownHandling.js
@@ -3,36 +3,31 @@
 describe('Cypress Basic', () => {
     beforeEach(() => {
         //Visit website
-      cy.visit('https://phptravels.net/')   
+      cy.visit('https://qavbox.github.io/demo/signup/')
     })
 
-    let language_xpath = "//button[@id='languages']"
-    let specificLanguage_xpath = "//div[@class='header-right-action pt-1 pe-2 multi_language']/div/ul/li"
-    
-
-
-    it('Drop Down handling ', function (){
-        this.skip()
-        cy.xpath(language_xpath).should('be.visible').click()
-        cy.xpath(specificLanguage_xpath).should('be.visible').click()
-        
-    })
-
-    it("By usinh select", function (){
-        this.skip()
-        cy.visit("https://www.facebook.com/")
-        cy.xpath("//select [@id- 'day']") . select ("15")
-    })
-
-    it("8y using without select tag", function(){
-        //cy.visit("https://www.phptravels.net/home")
-        cy.xpath(language_xpath).click()
-        cy.get('.multi_language > .dropdown > .dropdown-menu >').each(($el, index, $1ist)=>{
-        var lan =  $el.text()
-        if(lan == ' Arabic')
-        {
-            cy. wrap($el).click()
-        }
+    //Single Value Select
+    it('Drop Down handling part-01 ', function (){
+        cy.get('[name=\'sgender\']').select('male').invoke('val').then((value)=>{
+            cy.log('Selected Value = '+ value)
         })
-    })      
+        cy.wait(2000)
+        cy.get('[name=\'sgender\']').select('female').find('option:selected').should('have.text', 'Female')
+        cy.wait(2000)
+        cy.get('[name=\'sgender\']').select('na')
+        cy.wait(2000)
+        
+    })   
+
+    //Multiple Value Select
+    it('Drop Down handling part-02', function (){
+        cy.get('#tools').select(['selenium', 'postman', 'cypress']).find('option:selected').each(($e1)=>{
+            cy.wrap($e1).invoke('val').then((value)=>{
+                cy.log('Selected Value = '+ value)
+            }) 
+        })
+        cy.get('#tools').select(['selenium', 'postman', 'cypress']).invoke('val').should('deep.equal', ['selenium', 'postman', 'cypress'])
+        cy.wait(2000)
+        
+    }) 
 })
